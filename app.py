c69cca4e9a40602f11b0e98973dbeccda7005690
Tekvan1612 <vandanaajara@tekvan.in>
2025-03-11 12:52:47 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,33 +1,33 @@
 from flask import Flask, request, jsonify
 import psycopg2
 import json
-import config  # Import the config module
+import os
+import urllib.parse as urlparse
 
 app = Flask(__name__)
 
-
 def get_db_connection():
-    try:
-        print(f"Connecting to database {config.DB_NAME} at {config.DB_HOST} with user {config.DB_USER}")
-        conn = psycopg2.connect(
-            host=config.DB_HOST,
-            dbname=config.DB_NAME,
-            user=config.DB_USER,
-            password=config.DB_PASS
-        )
-        print("Database connection successful")
-        return conn
-    except Exception as e:
-        print(f"Error connecting to the database: {e}")
-        return None
+    DATABASE_URL = os.getenv('DATABASE_URL')
 
+    if not DATABASE_URL:
+        raise ValueError("DATABASE_URL not set!")
+
+    url = urlparse.urlparse(DATABASE_URL)
+
+    conn = psycopg2.connect(
+        dbname=url.path[1:],
+        user=url.username,
+        password=url.password,
+        host=url.hostname,
+        port=url.port,
+        sslmode='require'
+    )
+    print("Database connection successful!")
+    return conn
 
 def call_postgresql_function(action, params):
-    conn = get_db_connection()
-    if conn is None:
-        return None, {'message': 'Database connection failed', 'status': 0}
-
     try:
+        conn = get_db_connection()
         cur = conn.cursor()
         cur.execute("SELECT inventory_api(%s, %s::jsonb)", (action, json.dumps(params)))
         result = cur.fetchone()[0]
@@ -37,7 +37,6 @@ def call_postgresql_function(action, params):
     except Exception as e:
         print(f"Error executing query: {e}")
         return None, {'message': 'Internal server error', 'status': 0}
-
 
 @app.route('/login', methods=['POST'])
 def login():
@@ -53,7 +52,6 @@ def login():
     else:
         return jsonify({'message': 'Invalid credentials', 'status': 0}), 401
 
-
 @app.route('/jobs', methods=['GET'])
 def get_jobs_delivery_challan():
     action = 'get_jobs'
@@ -63,7 +61,6 @@ def get_jobs_delivery_challan():
         return jsonify(response), 500
 
     return jsonify({'jobs': result, 'status': 1}), 200
-
 
 @app.route('/jobs/<int:job_id>', methods=['GET'])
 def get_job_details_with_barcodes(job_id):
@@ -79,14 +76,13 @@ def get_job_details_with_barcodes(job_id):
 
     return jsonify({'job': result, 'status': 1}), 200
 
-
 @app.route('/scan_barcode', methods=['POST'])
 def scan_barcode():
     data = request.get_json()
-    print(f"Received data: {data}")  # Log received data
+    print(f"Received data: {data}")  
     action = 'insert_scanned_info'
     result, response = call_postgresql_function(action, data)
-    print(f"Result from PostgreSQL function: {result}")  # Log PostgreSQL function result
+    print(f"Result from PostgreSQL function: {result}")  
 
     if response['status'] == 0:
         return jsonify(response), 500
@@ -95,7 +91,6 @@ def scan_barcode():
         return jsonify({'message': 'Details not found', 'status': 0}), 404
 
     return jsonify(result), 200
-
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000, debug=True)
